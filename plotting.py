0dcd8d78a727c86a9b65a97c08b1d71f130ac760
Szymon Nakoneczny <snakoneczny@Szymons-MacBook-Pro.local>
2019-07-30 14:58:41 -0700

    cmb healpix map color

--- plotting.py
+++ plotting.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import healpy as hp
 
 
 def plot_map(hpxmap, unit='unit', is_cmap=True):
-    cmap = plt.get_cmap('hot_r') if is_cmap else None
-    hp.mollzoom(hpxmap, cmap=cmap, nest=False, xsize=1600, unit=unit, title='title')
+    # cmap = plt.get_cmap('hot_r') if is_cmap else None
+    mpl.rcParams['image.cmap'] = 'jet'
+    hp.mollzoom(hpxmap, nest=False, xsize=1600, unit=unit, title='title')
     hp.graticule()
